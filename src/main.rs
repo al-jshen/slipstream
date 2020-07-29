bdf308c1a81a1d97e3d701a4473cbd3f1378d4a8
Jeff Shen <jshen2014@hotmail.com>
2020-07-29 19:24:30 -0400

    query splitter

--- src/main.rs
+++ src/main.rs
@@ -2,31 +2,45 @@
 #[macro_use]
 extern crate rocket;
 use rocket::response::Redirect;
+use encoding::encode;
+
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
-#[get("/search/<query>")]
-fn search(query: String) -> Redirect {
-    let redir_url = "https://google.ca";
-    Redirect::to(redir_url)
+// #[get("/search/<query>")]
+// fn search(query: String) -> Redirect {
+//     let (cmd, args) = split_query(&query);
+//     match cmd {
+//         "ads" => process_ads(args), 
+//         _ => process_google(args),
+//     }
+//     let redir_url = "https://google.ca";
+//     Redirect::to(redir_url)
+// }
+
+fn process_google(args: String) -> String {
+    let base_url = "https://www.google.com/search?q=";
+    println!("{}", args);
+    let query = encode::encode(&args);
+    println!("{}", query);
+    format!("{}{}", base_url, query)
+
 }
 
-fn extract_command(query: &str) -> &str {
-    if let Some(command) = query.split_whitespace().next() {
-        return command;
-    } else {
-        return query;
-    }
-}
-
-fn split_query(query: &str) -> Vec<&str> {
-    query.split_ascii_whitespace().collect::<Vec<_>>()
+fn split_query(query: &str) -> (&str, String) {
+    let split = query.split_ascii_whitespace().collect::<Vec<_>>();
+    let cmd = split.first().unwrap();
+    let args = split[1..].join(" ");
+    (cmd, args)
 }
 
 fn main() {
+    let (cmd, args) = split_query("gh al-jshen/slipstream");
+    let g_url = process_google(args);
+    println!("{}", g_url);
     //rocket::ignite().mount("/", routes![index, search]).launch();
 }
 
@@ -36,24 +50,15 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_extract_command() {
-        assert_eq!(extract_command("gh al-jshen/slipstream"), "gh");
-        assert_eq!(extract_command("yt"), "yt");
-        assert_eq!(extract_command("yt "), "yt");
-        assert_eq!(extract_command(""), "");
-    }
-
-    #[test]
     fn test_split_query() {
         assert_eq!(
             split_query("gh al-jshen/slipstream"),
-            vec!["gh", "al-jshen/slipstream"]
+            ("gh", "al-jshen/slipstream".to_owned()),
         );
         assert_eq!(
             split_query("gh al-jshen slipstream"),
-            vec!["gh", "al-jshen", "slipstream"]
+            ("gh", "al-jshen slipstream".to_owned())
         );
-        assert_eq!(split_query("yt"), vec!["yt"]);
-        assert_eq!(split_query("yt "), vec!["yt"]);
+        assert_eq!(split_query("yt"), ("yt", "".to_owned()));
     }
 }
