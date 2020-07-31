9072fd8488e119ef45bc9a69a665d81215330738
Jeff Shen <jshen2014@hotmail.com>
2020-07-30 23:57:02 -0400

    more commands, documented help page

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,21 @@ use std::path::PathBuf;
 
 #[get("/")]
 fn index() -> &'static str {
-    "Hello, world!"
+    "slipstream"
+}
+
+#[get("/help")]
+fn help() -> &'static str {
+    "
+    Command \t Function            \t Examples \n
+    !help   \t This help page      \t also !list, ?
+    ads     \t NASA/ADS search     \t ads ^tacconi y:2013; ads carilli 2013 gas
+    gh      \t Github profile/repo \t gh al-jshen/slipstream; gh al-jshen
+    wk      \t Wikipedia search    \t wk united states; wk wikipedia
+    yt      \t Youtube search      \t yt rust tutorial; yt marritza ds9
+    maps    \t Google Maps search  \t maps new york city; maps VX2W+74; maps v0n 0a0; maps 32.4964474,-117.2286673
+    stk     \t Stock ticker lookup \t stk fb; stk qqq
+    "
 }
 
 #[get("/search/<query..>")]
@@ -19,8 +33,13 @@ fn search(query: PathBuf) -> Redirect {
 fn process_query(query: String) -> String {
     let (cmd, args) = split_query(&query);
     match cmd {
+        "!list" | "!help" | "?" => "/help".to_string(),
         "ads" => process_ads(args),
         "gh" => process_github(args),
+        "wk" => process_wikipedia(args),
+        "yt" => process_youtube(args),
+        "maps" => process_maps(args),
+        "stk" => process_stockprice(args),
         _ => process_google([cmd, &args].join(" ")),
     }
 }
@@ -43,6 +62,26 @@ fn process_ads(args: String) -> String {
     )
 }
 
+fn process_wikipedia(args: String) -> String {
+    let query = encode::encode(&args);
+    format!("https://en.wikipedia.org/wiki/{}", query)
+}
+
+fn process_youtube(args: String) -> String {
+    let query = encode::encode(&args);
+    format!("https://www.youtube.com/results?search_query={}", query)
+}
+
+fn process_maps(args: String) -> String {
+    let query = encode::encode(&args);
+    format!("https://www.google.ca/maps/search/{}", query)
+}
+
+fn process_stockprice(args: String) -> String {
+    let query = encode::encode(&args);
+    format!("https://finance.yahoo.com/quote/{}?p={}&.tsrc=fin-srch", query, query)
+}
+
 fn split_query(query: &str) -> (&str, String) {
     let split = query.split_ascii_whitespace().collect::<Vec<_>>();
     let cmd = split.first().unwrap();
@@ -52,5 +91,5 @@ fn split_query(query: &str) -> (&str, String) {
 
 fn main() {
     //println!("{}", process_query("gh al-jshen/slipstream".to_owned()));
-    rocket::ignite().mount("/", routes![index, search]).launch();
+    rocket::ignite().mount("/", routes![index, help, search]).launch();
 }
